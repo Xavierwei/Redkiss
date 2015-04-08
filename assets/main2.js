b64456a42851d13008d5e1ecbbb9bc97dceb7c96
ziwen-web <jziwenchen@gmail.com>
2015-04-08 14:52:35 +0800

    popup

--- assets/main2.js
+++ assets/main2.js
@@ -10,62 +10,124 @@
 		});
 	});
 
-    var isShow=false;
-    $('#sns_email').click(function(){
-        if(isShow==false){
-            $("#sns_email_popup").fadeIn();
-            isShow=true;
-        }else{
-            $("#sns_email_popup").fadeOut();
-            isShow=false;
-        }
-    })
+    //var isShow=false;
+    //$('#sns_email').click(function(){
+    //    if(isShow==false){
+    //        $("#sns_email_popup").fadeIn();
+    //        isShow=true;
+    //    }else{
+    //        $("#sns_email_popup").fadeOut();
+    //        isShow=false;
+    //    }
+    //})
+    //
+    //$('#sns_email_close').click(function(e){
+    //    $("#sns_email_popup").fadeOut();
+    //    e.stopPropagation();
+    //})
+    //
+    //$('#sns_form_email').focus(function(){
+    //    $(this).val('');
+    //    $('sns_form_email_validate').hide();
+    //})
+    //
+    //$('#sns_email_submit').click(function(){
+    //    var email=$('#sns_form_email').val()
+    //    var regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+    //    if(!regEmail.test(email)){
+    //        $('#sns_form_email_validate').show();
+    //        $('#sns_form_email').focus();
+    //        return;
+    //    }else{
+    //        $('#sns_form_email_validate').hide();
+    //    }
+    //    var allow_rcv=$('#sns_form_rcv').val();
+    //    var post_data={
+    //        email:email,
+    //        allow_rcv:allow_rcv
+    //    }
+    //    $.post(domain+'admin/user/email',post_data,function(response){
+    //        //if(response.status==0){
+    //        //    alert("succès!");
+    //        //}else{
+    //        //    response.error&&alert(response.error);
+    //        //    $('#sns_form_email').focus();
+    //        //}
+    //        alert("succès!");
+    //        $('#sns_email_popup').fadeOut();
+    //    }).error(function(){
+    //        alert("succès!");
+    //        $('#sns_email_popup').fadeOut();
+    //    })
+    //})
+    //
+    //$('.end_frame').click(function(e){
+    //    if(e.target==$(this)){
+    //        $('#sns_email_popup').fadeOut();
+    //        e.stopPropagation();
+    //    }
+    //});
 
-    $('#sns_email_close').click(function(e){
-        $("#sns_email_popup").fadeOut();
-        e.stopPropagation();
-    })
+    $(function () {
+        var emailBtn = $('#sns_email > a')
+            ,emailSct = $('#sns_email_popup')
+            ,sendBtn = $("#sns_email_submit")
+            ,emailCon = $('.end_list_wrap');
 
-    $('#sns_form_email').focus(function(){
-        $(this).val('');
-        $('sns_form_email_validate').hide();
-    })
+        var offset = emailBtn.offset();
+        emailBtn.click(function () {
+            // hide
+            if (emailBtn.data('shown')) {
+                emailBtn.data('shown', false);
+                emailSct.animate({
+                    opacity: 0
+                }, 500);
+            }
+            // show
+            else {
+                emailBtn.data('shown', true);
+                emailSct.css({'display': 'block', 'opacity': 0});
+                emailSct.animate({
+                    opacity: 1
+                }, 500);
+            }
+        });
 
-    $('#sns_email_submit').click(function(){
-        var email=$('#sns_form_email').val()
-        var regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
-        if(!regEmail.test(email)){
-            $('#sns_form_email_validate').show();
-            $('#sns_form_email').focus();
-            return;
-        }else{
-            $('#sns_form_email_validate').hide();
-        }
-        var allow_rcv=$('#sns_form_rcv').val();
-        var post_data={
-            email:email,
-            allow_rcv:allow_rcv
-        }
-        $.post(domain+'admin/user/email',post_data,function(response){
-            //if(response.status==0){
-            //    alert("succès!");
-            //}else{
-            //    response.error&&alert(response.error);
-            //    $('#sns_form_email').focus();
-            //}
-            alert("succès!");
-            $('#sns_email_popup').fadeOut();
-        }).error(function(){
-            alert("succès!");
-            $('#sns_email_popup').fadeOut();
-        })
-    })
+        function showSuccess() {
+            var els = []
+                ,con = $('.checkboxFive');
+            con.children().each(function () {
+                els.push($(this));
+            });
 
-    $('.end_frame').click(function(e){
-        if(e.target==$(this)){
-            $('#sns_email_popup').fadeOut();
-            e.stopPropagation();
+            var msg = $('<span class="email-success-msg">THANK YOU!</span>');
+            con.html(msg);
+
+            setTimeout(function () {
+                emailCon.fadeIn(500);
+            }, 2000);
         }
+
+        function showError(error){
+            error || (error = "ERROR HAPPENED, PLEASE TRY LATER");
+            var msg = $('<span style="opacity: 0;" class="email-error-msg">'+error+'</span>');
+
+            $('.checkboxFive').append(msg);
+            msg.animate({
+                opacity: 1
+            }, 200);
+        }
+
+        sendBtn.click(function () {
+            setTimeout(showSuccess, 500);
+        });
+
+        $('body').click(function (event) {
+            if (emailBtn.data('shown') &&
+            !$(event.target).parents('.end_list_wrap').size()) {
+                emailBtn.click();
+            }
+        });
     });
 
 }(jQuery));
---
+++ backend/info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jackeychen
+ * Date: 4/7/15
+ * Time: 4:12 PM
+ */
+
+phpinfo();
